bd3da12c56c1fbc486895d7be5bc9287dc6d1f10
Arthur Oliveira <arthur.oliveira@locaweb.com.br>
2016-06-22 16:42:10 -0300

    Add conf to fill-column-indicator

--- emacs/conf/init-fill-column-indicator.el
+++ emacs/conf/init-fill-column-indicator.el
@@ -2,10 +2,22 @@
 
 (setq fci-rule-width 1)
 (setq fci-rule-color "#222222")
-(setq fci-rule-column 80)
+(setq-default fci-rule-column 80)
+(setq fci-handle-truncate-lines nil)
+
+(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode 1)
+(defun auto-fci-mode (&optional unused)
+  (if (> (window-width) fci-rule-column)
+    (fci-mode 1)
+    (fci-mode 0))
+  )
+
+(add-hook 'after-change-major-mode-hook 'auto-fci-mode)
+(add-hook 'window-configuration-change-hook 'auto-fci-mode)
 
 ;; Add for ruby projects
-(add-hook 'ruby-mode-hook 'fci-mode)
+;;(add-hook 'ruby-mode-hook 'fci-mode)
 
 ;; To turn on fci-mode automatically for all files, put the following line
 ;;(add-hook 'after-change-major-mode-hook 'fci-mode)
